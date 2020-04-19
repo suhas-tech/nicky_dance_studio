0ee9107e6459e758d8a76f634f20dd5b9e06e102
Suhas <suhas@letslocalise.co.uk>
2020-04-19 17:45:54 +0530

    reverse proxy

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cookieParser = require("cookie-parser");
 const path = require("path");
 const mongoose = require("mongoose");
 const https = require("https");
-
+var http=require('http');
 // const options = {
 //   key: fs.readFileSync("key.pem"),
 //   cert: fs.readFileSync("cert.pem"),
@@ -30,14 +30,24 @@ mongoose.connect(mongo_uri, { useNewUrlParser: true }, function (err) {
 
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+var server=http.createServer(function(req,res){
+  res.end('test');
 });
 
-const port = 8001
-      app.listen(port, ()=>{
-          console.log('listening at port 8443')
+server.on('listening',function(){
+  console.log('ok, server is running');
 });
+
+server.listen(80);
+
+// app.get("/*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
+
+// const port = 8001
+//       app.listen(port, ()=>{
+//           console.log('listening at port 8443')
+// });
 
 // https
 //   .createServer(options, function (req, res) {
