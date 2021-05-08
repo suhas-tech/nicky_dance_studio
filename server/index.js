fa0a1ccb9e35c3a23a2e3c7e5acf634e4a4d9280
Suhas <suhas@letslocalise.co.uk>
2021-05-09 02:34:25 +0530

    client from the server

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 //   }
 // });
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "../client", "build")));
 
 app.get("/files/:id", function (req, res) {
   res.sendFile(path.join(__dirname, "files", req.params.id));
